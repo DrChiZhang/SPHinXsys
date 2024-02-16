2ee8ff386ed6551b7976923da9c572092cb70f4e
ChiZhangatTUM <zhangchi0118@gmail.com>
2024-02-16 19:07:01 +0100

    modify header file.

--- src/shared/tools/polygon_mesh.h
+++ src/shared/tools/polygon_mesh.h
@@ -1,24 +1,29 @@
-/* -------------------------------------------------------------------------*
- *                                SPHinXsys                                 *
- * -------------------------------------------------------------------------*
- *                                                                          *
- * Copyright (c) 2023, The AUTHORS.                                         *
- *                                                                          *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may  *
- * not use this file except in compliance with the License. You may obtain a*
- * copy of the License at                                                   *
- *          http://www.apache.org/licenses/LICENSE-2.0.                     *
- * Unless required by applicable law or agreed to in writing, software      *
- * distributed under the License is distributed on an "AS IS" BASIS,        *
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
- * See the License for the specific language governing permissions and      *
- * limitations under the License.                                           *
- * ------------------------------------------------------------------------*/
-
+/* ------------------------------------------------------------------------- *
+ *                                SPHinXsys                                  *
+ * ------------------------------------------------------------------------- *
+ * SPHinXsys (pronunciation: s'finksis) is an acronym from Smoothed Particle *
+ * Hydrodynamics for industrial compleX systems. It provides C++ APIs for    *
+ * physical accurate simulation and aims to model coupled industrial dynamic *
+ * systems including fluid, solid, multi-body dynamics and beyond with SPH   *
+ * (smoothed particle hydrodynamics), a meshless computational method using  *
+ * particle discretization.                                                  *
+ *                                                                           *
+ * SPHinXsys is partially funded by German Research Foundation               *
+ * (Deutsche Forschungsgemeinschaft) DFG HU1527/6-1, HU1527/10-1,            *
+ *  HU1527/12-1 and HU1527/12-4.                                             *
+ *                                                                           *
+ * Portions copyright (c) 2017-2023 Technical University of Munich and       *
+ * the authors' affiliations.                                                *
+ *                                                                           *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may   *
+ * not use this file except in compliance with the License. You may obtain a *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.        *
+ *                                                                           *
+ * ------------------------------------------------------------------------- */
 /**
  * @file 	Polygon_mesh.h
  * @brief 	Polygon mesh, deal with OBJ file parser with writer, loader and parser.
- * @author	Chi ZHang Email: zhangchi0118@gmail.com
+ * @author	Chi Zhang
  */
 
 #ifndef POLYGON_MESH_H
